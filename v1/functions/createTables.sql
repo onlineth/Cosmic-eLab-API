ad52f06bd9dbe4916dc558f0513b915d704731f7
Thomas <thomas@onlineth.com>
2016-08-01 11:44:05 -0500

    Add auto inc

--- v1/functions/createTables.sql
+++ v1/functions/createTables.sql
@@ -1,10 +1,12 @@
-CREATE TABLE api_files
-(
-    fileid INTEGER PRIMARY KEY NOT NULL,
-    detectorid INTEGER NOT NULL,
-    year INTEGER NOT NULL,
-    monthday INTEGER NOT NULL,
-    index INTEGER NOT NULL,
-    filetype CHAR(10) NOT NULL,
-    active BOOLEAN DEFAULT true NOT NULL
+CREATE TABLE public.api_files (
+  fileid INTEGER PRIMARY KEY NOT NULL,
+  detectorid INTEGER NOT NULL,
+  year INTEGER NOT NULL,
+  monthday INTEGER NOT NULL,
+  index INTEGER NOT NULL,
+  filetype CHARACTER(10) NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT true
 );
+CREATE SEQUENCE public.api_files_fileid_seq NO MINVALUE NO MAXVALUE NO CYCLE;
+ALTER TABLE public.api_files ALTER COLUMN fileid SET DEFAULT nextval('public.api_files_fileid_seq');
+ALTER SEQUENCE public.api_files_fileid_seq OWNED BY public.api_files.fileid;
